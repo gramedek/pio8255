fe6b42b320a7d421a0ba22b3ade1d13607320e11
Georges-Emile Bihr <ge-bihr@pyrokinesis.fr>
2021-01-09 19:50:31 +0100

    added comments to pio8255.h

--- pio8255.h
+++ pio8255.h
@@ -6,7 +6,22 @@
 class PIO8255
 {
   public:
-    PIO8255(int d0 = 2,int d1 = 3,int d2 = 4,int d3 = 5,int d4 = 6,int d5 = 7,int d6 = 8,int d7 = 9,int cs = 10,int rd = 16,int wr = 14,int res = A2,int a0 = A0,int a1 = A1);
+    PIO8255(
+		int d0 = 2,
+		int d1 = 3,
+		int d2 = 4,
+		int d3 = 5,
+		int d4 = 6,
+		int d5 = 7,
+		int d6 = 8,
+		int d7 = 9,
+		int cs = 10,
+		int rd = 16,
+		int wr = 14,
+		int res = A2,
+		int a0 = A0,
+		int a1 = A1
+		);
     void init();
     void set_port(char port, byte data);
     void set_pin(char port, int pin, boolean highlow);
@@ -16,6 +31,33 @@ class PIO8255
     int out_data[8];
     byte porta,portb,portc;
 };
+
+/*
+	     ┌──────┐ ┌──────┐
+	PA3  │  1   └─┘   40 │  PA4
+	PA2  │  2         39 │  PA5
+	PA1  │  3         38 │  PA6
+	PA0  │  4         37 │  PA7
+	 RD  │  5         36 │  WR
+	 CS  │  6         35 │  Reset
+	GND  │  7         34 │  D0
+	 A1  │  8         33 │  D1
+	 A0  │  9         32 │  D2
+	PC7  │ 10         31 │  D3
+	PC6  │ 11         30 │  D4
+	PC5  │ 12         29 │  D5
+	PC4  │ 13         28 │  D6
+	PC0  │ 14         27 │  D7
+	PC1  │ 15         26 │  Vcc
+	PC2  │ 16         25 │  PB7
+	PC3  │ 17         24 │  PB6
+	PB0  │ 18         23 │  PB5
+	PB1  │ 19         22 │  PB4
+	PB2  │ 20         21 │  PB3
+	     └───────────────┘
+
+
+*/
 
 
 
